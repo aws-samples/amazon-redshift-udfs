0d74c466d110abd5d75eea24f02e5cbb71e80371
Rajiv Gupta <rjvgupta@amazon.com>
2021-08-09 16:56:15 -0700

    test cfn

--- bin/deployFunction.sh
+++ bin/deployFunction.sh
@@ -49,7 +49,7 @@ if test -f "../$category/$function/lambda.yaml"; then
   stackname=${stackname//)/}
   stackname=${stackname//_/-}
   stackname=${stackname//,/-}
-  aws cloudformation update-stack --stack-name ${stackname} --parameters ParameterKey=LambdaRole,ParameterValue=$iamRole --template-body "$template" || aws cloudformation create-stack --on-failure DELETE --stack-name ${stackname} --parameters ParameterKey=LambdaRole,ParameterValue=$iamRole --template-body "$template"
+  aws cloudformation update-stack --stack-name ${stackname} --parameters ParameterKey=LambdaRole,ParameterValue=$iamRole --no-fail-on-empty-changeset --template-body "$template" || aws cloudformation create-stack --on-failure DELETE --stack-name ${stackname} --parameters ParameterKey=LambdaRole,ParameterValue=$iamRole --template-body "$template"
   aws cloudformation wait stack-create-complete --stack-name ${stackname}
   sqlparm=`echo --parameters "[{\"name\":\"iamRole\",\"value\":\"$iamRole\"}]"`
 fi
