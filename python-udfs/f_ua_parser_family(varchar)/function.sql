8d098b0a351f2dd10792b7228a39e7bdfcaa7328
Rajiv Gupta <rjvgupta@amazon.com>
2021-08-09 07:28:07 -0700

    test deploy

--- python-udfs/f_ua_parser_family(varchar)/function.sql
+++ python-udfs/f_ua_parser_family(varchar)/function.sql
@@ -4,7 +4,7 @@ Purpose: This function will extract from the user_agent string. This function de
 2021-08-08: written by rjvgupta
 */
 --
-CREATE OR REPLACE FUNCTION parse_ua_family (ua VARCHAR)
+CREATE OR REPLACE FUNCTION f_ua_parser_family (ua VARCHAR)
 RETURNS VARCHAR IMMUTABLE AS $$
   from ua_parser import user_agent_parser
   return user_agent_parser.ParseUserAgent(ua)['family']
