1f235eced54d749717b6ac1d54c3911c59133c6d
Rajiv Gupta <rjvgupta@amazon.com>
2021-08-09 21:18:10 -0700

    new fns

--- lambda-udfs/f_upper_python(varchar)/function.sql
+++ lambda-udfs/f_upper_python(varchar)/function.sql
@@ -2,9 +2,6 @@
 Purpose:
     This sample function demonstrates how to create/use lambda UDFs in python
 
-Internal dependencies: None
-External dependencies: None
-
 2021-08-01: written by rjvgupta
 */
 CREATE OR REPLACE EXTERNAL FUNCTION f_upper_python(varchar) RETURNS varchar IMMUTABLE
---
+++ python-udfs/f_parse_url_query_string(varchar)/function.sql
@@ -0,0 +1,14 @@
+/* Purpose: This UDF takes a URL as an argument, and parses out the field-value pairs.
+Returns pairs in JSON for further parsing if needed.
+
+2015-09-10: written by chriz@
+*/
+
+CREATE OR REPLACE FUNCTION f_parse_url_query_string(url VARCHAR(MAX))
+RETURNS varchar(max)
+STABLE
+AS $$
+    from urlparse import urlparse, parse_qsl
+    import json
+    return json.dumps(dict(parse_qsl(urlparse(url)[4])))
+$$ LANGUAGE plpythonu;
