45bbe05b448d0bbce8b011f906eb5c3b0780371c
Rajiv Gupta <rjvgupta@amazon.com>
2021-08-09 22:28:34 -0700

    redeploy

--- python-udfs/f_unixts_to_timestamp(bigint,char(2))/function.sql
+++ python-udfs/f_unixts_to_timestamp(bigint,char(2))/function.sql
@@ -1,9 +1,9 @@
 /* Purpose: Converts a UNIX timestamp to a UTC datetime with up to microseconds granularity.
- 
+
 2015-09-10: written by chriz@
 */
 
-CREATE OR REPLACE FUNCTION f_unixts_to_timestamp(ts BIGINT, units CHAR(2))
+CREATE OR REPLACE FUNCTION f_unixts_to_timestamp(ts BIGINT, units varchar)
 RETURNS timestamp
 STABLE
 AS $$
