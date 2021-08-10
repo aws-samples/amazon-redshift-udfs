9f51b680b47e04ff524e6d63832a185692bbc170
Rajiv Gupta <rjvgupta@amazon.com>
2021-08-09 21:23:29 -0700

    test deploy

--- python-udfs/f_parse_url_query_string(varchar)/function.sql
+++ python-udfs/f_parse_url_query_string(varchar)/function.sql
@@ -1,6 +1,6 @@
 /* Purpose: This UDF takes a URL as an argument, and parses out the field-value pairs.
 Returns pairs in JSON for further parsing if needed.
- 
+
 2015-09-10: written by chriz@
 */
 
