c2e834d43926a46c121a749a5c3c9381fb065411
Jon Roberts <jgrcloud@amazon.com>
2023-08-11 14:19:29 -0500

    Levenshtein fixes

--- lambda-udfs/fn_lambda_levenshtein_distance(varchar,varchar)/function.sql
+++ lambda-udfs/fn_lambda_levenshtein_distance(varchar,varchar)/function.sql
@@ -1,3 +1,2 @@
 create or replace external function fn_lambda_levenshtein_distance(a varchar, b varchar) returns int 
 lambda 'fn_lambda_levenshtein_distance' iam_role ':RedshiftRole' immutable retry_timeout 0;
-
