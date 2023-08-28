a3764142007f3eacac4e7875c511664f1976a2d3
Rajiv Gupta <rjvgupta@amazon.com>
2023-08-28 15:41:56 -0700

    Update function.sql

--- lambda-udfs/f_glue_schema_registry_avro_to_json(varchar,varchar,varchar)/function.sql
+++ lambda-udfs/f_glue_schema_registry_avro_to_json(varchar,varchar,varchar)/function.sql
@@ -2,7 +2,7 @@
 Purpose:
     This sample function demonstrates how to create/use lambda UDFs in python to convert Avro encoded data into JSON for use in
     Redshift SUPER data type using the Glue Schema Registry.
-    TODO: Link to blog.
+    TODO: Link to blog
 
 2023-08-25: written by mmehrten
 */ 
