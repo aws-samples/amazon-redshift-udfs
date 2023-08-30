ea5e5336eee47aa427dec913196740c42a72b1bf
Rajiv Gupta <rjvgupta@amazon.com>
2023-08-30 16:20:26 -0700

    Update function.sql

--- lambda-udfs/f_kms_encrypt(varchar,varchar)/function.sql
+++ lambda-udfs/f_kms_encrypt(varchar,varchar)/function.sql
@@ -3,7 +3,7 @@ Purpose:
     This sample function demonstrates how to encrypt data which was encrypted using a KMS key.
     This function can be used in conjunction with f_kms_decrypt.
 
-    Note: the test input/output is for illustration and would need to be modified to use data that was encrypted with YOUR kms key.
+    Note: the test input/output is for illustration and would need to be modified to use data that was encrypted with YOUR kms key
 
 2021-09-08: written by rjvgupta
 */
