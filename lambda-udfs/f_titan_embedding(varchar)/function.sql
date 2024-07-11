4982a7f6c98ad36968e88947c2d4d5a605a4b786
rjvgupta@amazon.com <rjvgupta@amazon.com>
2024-07-11 15:58:52 -0700

    try titan embedding

---
+++ lambda-udfs/f_titan_embedding(varchar)/function.sql
@@ -0,0 +1,8 @@
+/*
+Purpose:
+    This sample function demonstrates how use lambda to call the titan embedding model to convert your text to an embedding.
+
+2024-07-11: written by rjvgupta
+*/ 
+CREATE OR REPLACE EXTERNAL FUNCTION f_titan_embedding (varchar) RETURNS varchar STABLE
+LAMBDA 'f-titan-embedding-varchar' IAM_ROLE ':RedshiftRole';
