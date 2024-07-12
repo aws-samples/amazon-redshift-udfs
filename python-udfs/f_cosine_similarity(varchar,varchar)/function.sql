668277b885db033e148189018ed790a1dec1e975
rjvgupta@amazon.com <rjvgupta@amazon.com>
2024-07-11 22:02:35 -0700

    fixed function

--- python-udfs/f_cosine_similarity(varchar,varchar)/function.sql
+++ python-udfs/f_cosine_similarity(varchar,varchar)/function.sql
@@ -4,11 +4,11 @@ Purpose: This function use numpy to determine the similary between two vectors.
 2021-08-08: written by rjvgupta
 */
 --
-CREATE OR REPLACE FUNCTION f_cosine_similarity (v1 VARCHAR, v2 VARCHAR)
-RETURNS VARCHAR IMMUTABLE AS $$
-  import numpy as np
+CREATE OR REPLACE FUNCTION f_cosine_similarity (v1 VARCHAR(MAX), v2 VARCHAR(MAX))
+RETURNS FLOAT8 IMMUTABLE AS $$
+  import numpy,json
   from numpy.linalg import norm
-  A = np.array(json.loads(v1))
-  B = np.array(json.loads(v2))
-  return np.dot(A,B)/(norm(A)*norm(B))
+  A = numpy.array(json.loads(v1))
+  B = numpy.array(json.loads(v2))
+  return numpy.dot(A,B)/(norm(A)*norm(B))
 $$ LANGUAGE plpythonu;
