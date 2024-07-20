92e16d1edd4f0411588452009ecf4c9ff4c08bb7
Rajiv Gupta <rjvgupta@amazon.com>
2024-07-19 21:17:34 -0700

    Delete lambda-udfs/f_fastembed_embedding(varchar) directory

--- lambda-udfs/f_fastembed_embedding(varchar)/function.sql
+++
@@ -1,8 +0,0 @@
-/*
-Purpose:
-    This sample function demonstrates how use lambda to call the titan embedding model to convert your text to an embedding.
-
-2024-07-11: written by rjvgupta
-*/ 
-CREATE OR REPLACE EXTERNAL FUNCTION f_fastembed_embedding (varchar) RETURNS varchar(max) STABLE
-LAMBDA 'f-fastembed-embedding-varchar' IAM_ROLE ':RedshiftRole';
